a66ebc3950122ee2b0cc0b8f158a31320c356bb3
Pablo Recio Quijano <precio@yaco.es>
2010-08-27 13:15:12 +0200

    Added RemoteTask class to abstract the data extracted in Hamster with the ones that will be sended to the Remote Server

---
+++ src/cobaya/remote_task.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# any later version.                                                          #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+# Copyright (C) 2010, Pablo Recio Quijano, <precio@yaco.es>                   #
+###############################################################################
+
+import json
+
+
+class RemoteTask(object):
+    """ Represent a task ready to be send to a remote server
+    """
+
+
+    def __init__(self, task_id, ticket_number, project,
+                 date, time, remote_sync=False):
+        self.task_id = task_id
+        self.ticket_number = ticket_number
+        self.project = project
+        self.date = date
+        self.time = time / 60 / 24 # in hoursº
+        self.remote_sync = remote_sync
+
+    def to_dict(self):
+        data = {'ticket': self.ticket_number, 'project': self.project,
+                'date': self.date, 'time': self.time}
+        return data
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
