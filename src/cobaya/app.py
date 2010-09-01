b1e1a91dd0dbd1b356de331b8d22493e025d218f
Lorenzo Gil <lorenzo.gil.sanchez@gmail.com>
2010-09-02 01:31:42 +0800

    Update copyright holders

--- src/cobaya/app.py
+++ src/cobaya/app.py
@@ -12,7 +12,8 @@
 # You should have received a copy of the GNU General Public License           #
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #                                                                             #
-# Copyright (C) 2010, Lorenzo Gil Sanchez <lgs@yaco.es>                       #
+# Copyright (C) 2010, Pablo Recio Quijano <precio@yaco.es>                    #
+#               2010, Lorenzo Gil Sanchez <lgs@yaco.es>                       #
 ###############################################################################
 
 from os import path
